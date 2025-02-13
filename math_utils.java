c667fb567a48b03bcfae32794d79b03242a76b9e
Alejandro Diest <alejandrodiestsanz@gmail.com>
2025-02-13 19:27:14 +0100

    Version 1.1.0

--- math_utils.java
+++ math_utils.java
@@ -1,10 +1,14 @@
-// math_utils.java
+
 public class math_utils {
     public static int add(int a, int b) {
         return a + b;
     }
 
     public static int subtract(int a, int b) {
-        return a - b;
+        return a + b;
+    }
+    
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 }
